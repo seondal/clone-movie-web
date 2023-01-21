8cc86d95799247e8b3a5a7bebae5489f409e06fb
seondal <sseondal@gmail.com>
2023-01-22 02:10:31 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,5 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  // const byeFn = () => {
-  //   console.log("bye :(");
-  // };
-  // const hiFn = () => {
-  //   console.log("hi :)");
-  //   return byeFn;
-  // };
-  // useEffect(hiFn, []);
-
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
-  return (
-    <div>
-      {showing ? <Hello /> : <></>}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
