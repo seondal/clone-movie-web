14b70a13e9b3eeb916e3f94d84ff7b6174e00fa2
seondal <sseondal@gmail.com>
2023-01-22 02:37:27 +0900

    clone : todo list app (simple ver)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 function App() {
-  return <div>hello</div>;
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (e) => setTodo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodo("");
+    setTodos((cur) => [...cur, todo]);
+  };
+
+  return (
+    <div>
+      <h1> My To Dos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {todos.map((todo, idx) => (
+        <li key={idx}>{todo}</li>
+      ))}
+    </div>
+  );
 }
 
 export default App;
