5a2eb745498fdccd76321f1fc06b1057a9037471
seondal <sseondal@gmail.com>
2023-02-01 03:32:07 +0900

    #7.4 Movie App Part two

--- src/App.js
+++ src/App.js
@@ -1,47 +1,5 @@
-import { useEffect, useState } from "react";
+import Home from "./routes/Home";
 
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=5&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g, idx) => (
-                  <li key={idx}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+export default function App() {
+  return null;
 }
-
-export default App;
