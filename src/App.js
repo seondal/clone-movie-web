b83ed760df561e846de27d1a744fecac5e537316
seondal <sseondal@gmail.com>
2023-02-01 03:19:25 +0900

    async-await 정리

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
   const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    );
-    const json = (response) => response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=5&sort_by=year`
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
@@ -17,7 +18,30 @@ function App() {
     getMovies();
   }, []);
 
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  console.log(movies);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g, idx) => (
+                  <li key={idx}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
