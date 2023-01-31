12db0381b35fe8be469eb289c73de81dfbe15d75
seondal <sseondal@gmail.com>
2023-02-01 03:12:03 +0900

    async-await

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    fetch(
+  const getMovies = async () => {
+    const response = await fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    );
+    const json = (response) => response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
   }, []);
-  console.log(movies);
+
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
